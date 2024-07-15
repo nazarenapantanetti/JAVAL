364439923d3374c5697c741bf6c837affd183cb8
Nazarena Pantanetti <nazapantanetti@gmail.com>
2024-07-15 11:27:19 -0300

    Ejercitacion clase 3

---
+++ src/main/java/clase03/Ejercicio01.java
@@ -0,0 +1,43 @@
+package clase03;
+import com.github.sanchezih.util.io.Consola;
+/* Programa que lee la altura de N personas y calcula:
+La altura media.
+Cuantas personas tienen una altura superior a la media.
+Cuantas tienen una altura inferior a la media.
+ 
+El valor de N se pide por teclado y debe ser entero positivo.
+ */
+public class Ejercicio01 {
+	public static void main(String[] args) {
+		int numPersonas, superiorMedia = 0, inferiorMedia = 0;
+		double sumAlturas = 0, media = 0;
+
+		do {
+			System.out.print("Introduce el numero de personas: ");
+			numPersonas = Consola.leerEntero();
+			if (numPersonas <= 0) {
+				System.out.println("El numero debe ser un entero positivo. Intenta de nuevo.");
+			}
+		} while (numPersonas <= 0);
+
+		double[] alturas = new double[numPersonas];
+
+		for (int i = 0; i < numPersonas; i++) {
+			System.out.print("Ingrese altura de la persona " + (i + 1) + ": ");
+			alturas[i] = Consola.leerDouble();
+			sumAlturas += alturas[i];
+		}
+		media = sumAlturas / numPersonas;
+		for (double altura : alturas) {
+			if (altura > media) {
+				superiorMedia++;
+			} else if (altura < media) {
+				inferiorMedia++;
+			}
+
+		}
+		System.out.printf("Altura media: %.2f metros\n", media);
+		System.out.println("Personas con altura superior a la media: " + superiorMedia);
+		System.out.println("Personas con altura inferior a la media: " + inferiorMedia);
+	}
+}
---
+++ src/main/java/clase03/Ejercicio02.java
@@ -0,0 +1,48 @@
+package clase03;
+
+import com.github.sanchezih.util.io.Consola;
+
+/*Programa que pide 10 numeros por teclado, los almacena en un array y que luego muestra
+ * el maximo valor, el minimo y las posiciones que ocupan en el array.
+ */
+public class Ejercicio02 {
+	public static void main(String[] args) {
+		int ubicacionMax = 0, ubicacionMin = 0;
+		System.out.print("Ingrese 10 num: ");
+		int[] numeros = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Ingrese el número " + (i + 1) + ": ");
+			numeros[i] = Consola.leerEntero();
+		}
+		// Identifico valor maximo
+		int max = numeros[0];
+		for (int i = 1; i < 10; i++) {
+			if (numeros[i] > max) {
+				max = numeros[i];
+			}
+		}
+		// Identifico ubicacion valor maximo
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] == max) {
+				ubicacionMax = i;
+			}
+		}
+		System.out.println("El maximo vallor es " + max + " cuya ubicacion es " + ubicacionMax);
+
+		// Identifico valor minimo
+		int min = numeros[0];
+		for (int i = 1; i < 10; i++) {
+			if (numeros[i] < min) {
+				min = numeros[i];
+			}
+		}
+		// Identifico ubicacion valor minimo
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] == min) {
+				ubicacionMin = i;
+			}
+		}
+			System.out.println("El maximo vallor es " + min + " cuya ubicacion es " + ubicacionMin);
+	}
+}
